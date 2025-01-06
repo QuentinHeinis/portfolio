559e0d0d3d74d9c1da7f46defe645e510fb0af39
Quentin Heinis <quentin@heinis.dev>
2025-01-06 21:47:21 +0100

    add transition

--- app.vue
+++ app.vue
@@ -1,15 +1,7 @@
 <script setup>
 import Lenis from "lenis";
-
-
+import gsap from "gsap";
 const footerId = useState("footerId");
-
-const nuxtApp = useNuxtApp();
-
-nuxtApp.hook("page:finish", () => {
-  window.scrollTo(0, 0);
-  
-});
 
 onMounted(() => {
   const lenis = useState("lenis", () => new Lenis());
@@ -22,14 +14,57 @@ onMounted(() => {
 
   console.log("made with ❤️ by Quentin Heinis");
 });
+
+const transitionScreen = ref();
+const router = useRouter();
+router.beforeEach((to, from, next) => {
+  // Animation de l'écran rouge sortant avant de changer de page
+  gsap.to(transitionScreen.value, {
+    x: "0%",
+    duration: 0.5,
+  });
+  gsap.to(transitionScreen.value, {
+    x: "100%",
+    duration: 1,
+    delay: 0.8,
+    onStart: () => {
+      next();
+      setTimeout(() => {
+        if (!to.hash) window.scrollTo(0, 0);
+      }, 250);
+    },
+  });
+  gsap.to(transitionScreen.value, {
+    x: "-100%",
+    duration: 0,
+    delay: 1.8,
+  });
+});
 </script>
 <template>
   <div>
     <MyCursor />
     <MyHeader />
     <main>
+      <div
+        ref="transitionScreen"
+        class="transition-screen"
+        aria-hidden="true"
+      ></div>
       <NuxtPage />
     </main>
     <MyFooter :id="footerId" />
   </div>
 </template>
+<style scoped>
+.transition-screen {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 100vh;
+  transform: translate(-100%, 0);
+  background-color: #f15a52;
+  z-index: 1000;
+}
+</style>
